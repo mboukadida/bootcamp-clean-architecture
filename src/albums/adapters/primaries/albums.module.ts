c438f1dd5fdad9e539e0707b7c234a7b7829e7dc
Majed BOUKADIDA <majed.boukadida@gmail.com>
2020-12-13 23:57:17 +0100

    Add album factory repository

--- src/albums/adapters/primaries/albums.module.ts
+++ src/albums/adapters/primaries/albums.module.ts
@@ -3,12 +3,12 @@ import { AlbumItemComponent } from './albums-list/album-item/album-item.componen
 import { AlbumsRoutingModule } from './albums-routing.module';
 import { AlbumDetailsComponent } from './album-details/album-details.component';
 import { GetAllAlbums } from 'src/albums/use-cases/get-all-albums';
-import { AlbumRepositoryFactory } from '../secondaries/in-memory/repository-factory';
+import { AlbumRepositoryFactory } from '../secondaries/configurations/repository-factory';
 import { GetAlbumDetails } from 'src/albums/use-cases/geta-album-details';
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AlbumsListComponent, AlbumItemComponent, AlbumDetailsComponent],
